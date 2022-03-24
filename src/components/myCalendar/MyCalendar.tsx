04b4fd6ed7c911b94a4f30f35c2f4636825fd78e
huanshenyi <txy1226052@gmail.com>
2022-03-24 19:28:26 +0900

    マイカレンダのテンプレート追加

--- src/components/myCalendar/MyCalendar.tsx
+++ src/components/myCalendar/MyCalendar.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Calendar, Badge } from "antd";
+import ja from "antd/lib/calendar/locale/ja_JP";
+import styles from "./MyCalendar.module.css";
 
 export const MyCalendar: React.FC = () => {
-  return <div>MyCalendar</div>;
+  return (
+    <>
+      <div className={styles["my-calendar"]}>
+        <Calendar locale={ja} />
+      </div>
+    </>
+  );
 };
