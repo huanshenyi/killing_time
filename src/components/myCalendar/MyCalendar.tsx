a179256cdc48a87d74ef6400f6a1fc5593d58b9f
huanshenyi <txy1226052@gmail.com>
2022-03-24 15:10:48 +0900

    mycalendarの初期化

---
+++ src/components/myCalendar/MyCalendar.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const MyCalendar: React.FC = () => {
+  return <div>MyCalendar</div>;
+};
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import { MyCalendar } from "../../components";
+
 import styles from "./HomePage.module.css";
 
 export const HomePage: React.FC = () => {
-  return <div className={styles["page-content"]}>home page</div>;
+  return (
+    <>
+      <div className={styles["page-content"]}>
+        <MyCalendar />
+      </div>
+    </>
+  );
 };
