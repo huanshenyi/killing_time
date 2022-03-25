e52b1d1d33536bf77378e876c015911174d548e7
huanshenyi <txy1226052@gmail.com>
2022-03-25 12:00:18 +0900

    routerの初期化

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { HomePage } from "./pages";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Board, HomePage } from "./pages";
+
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div className={styles.App}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Board />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
