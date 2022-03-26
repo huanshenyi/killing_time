59fe4ce4c5c5dcfa370aeeb2e89e7d7410dba6d8
huanshenyi <txy1226052@gmail.com>
2022-03-26 23:18:24 +0900

    timeLineのデザイン調整

--- src/components/timeLine/TimeLine.tsx
+++ src/components/timeLine/TimeLine.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { Card, Timeline } from "antd";
+import { Card, Timeline, Space } from "antd";
+import {
+  SolutionOutlined,
+  ProfileOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 
 import styles from "./TimeLine.module.css";
 
@@ -9,26 +14,34 @@ export const TimeLine: React.FC = () => {
       <Card
         title="直近タイムライン"
         extra={
-          <div>
-            <div>
-              <span className="ant-timeline-item-head ant-timeline-item-head-red" />
-              タスク
-            </div>
-            <div>
-              <span className="ant-timeline-item-head ant-timeline-item-head-green" />
-              依頼
-            </div>
-          </div>
+          <Space align="start" size={5}>
+            <SolutionOutlined style={{ fontSize: "16px", color: "red" }} />
+            <div>応募</div>
+            <ProfileOutlined style={{ fontSize: "16px", color: "green" }} />
+            <div>募集</div>
+          </Space>
         }
       >
         <Timeline mode={"left"}>
-          <Timeline.Item label="2015-09-01" color="red">
+          <Timeline.Item
+            label="2015-09-01"
+            dot={<SolutionOutlined style={{ fontSize: "16px" }} />}
+            color="red"
+          >
             Create a services
           </Timeline.Item>
-          <Timeline.Item label="2015-09-01 09:12:11" color="green">
-            Solve initial
+          <Timeline.Item
+            label="2015-09-01 09:12:11"
+            color="green"
+            dot={<ProfileOutlined style={{ fontSize: "16px" }} />}
+          >
+            <div>Solve initial</div>
           </Timeline.Item>
-          <Timeline.Item label="2015-09-01 09:12:11" color="red">
+          <Timeline.Item
+            label="2015-09-01 09:12:11"
+            color="green"
+            dot={<ProfileOutlined style={{ fontSize: "16px" }} />}
+          >
             Network problems
           </Timeline.Item>
         </Timeline>
