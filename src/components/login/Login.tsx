ece452c4982038a82eed825cc7c568048c800465
huanshenyi <txy1226052@gmail.com>
2022-04-22 18:25:52 +0900

    login modal item基本レイアウト完成

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,5 +1,6 @@
 import styles from "./Login.module.css";
-import { Row, Col } from "antd";
+import { Row, Col, Button } from "antd";
+import { CloseOutlined } from "@ant-design/icons";
 
 interface IProps {
   isShow: boolean;
@@ -7,12 +8,52 @@ interface IProps {
 }
 
 export const Login: React.FC<IProps> = ({ isShow, onClose }) => {
+  const handelClose = () => {
+    onClose && onClose();
+  };
   return isShow ? (
     <div className={styles.loginArea}>
       <div className={styles.loginBox}>
-        <Row>
+        <Row style={{ lineHeight: "31px" }}>
           <Col span={2} offset={22}>
-            x
+            <div className={styles.close} onClick={handelClose}>
+              <CloseOutlined />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={12} offset={6}>
+            <div className={styles.loginIcon}>iconここに置く</div>
+          </Col>
+        </Row>
+        <Row style={{ lineHeight: "31px" }}>
+          <Col span={24}>
+            <input className={styles.loginInput} placeholder="email" />
+          </Col>
+        </Row>
+        <Row style={{ lineHeight: "31px" }}>
+          <Col span={24}>
+            <input className={styles.loginInput} placeholder="password" />
+          </Col>
+        </Row>
+        <Row style={{ lineHeight: "32px" }}>
+          <Col span={24}>
+            <Button type="primary" size="large">
+              Login
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button>google login</Button>
+          </Col>
+          <Col span={8}>
+            <Button>github login</Button>
+          </Col>
+        </Row>
+        <Row style={{ lineHeight: "32px" }}>
+          <Col>
+            <div className={styles.loginPrivacy}>xxxxxxxxx</div>
           </Col>
         </Row>
       </div>
