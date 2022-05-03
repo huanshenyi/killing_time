abe356df11a0cd9e9670380cf9a2ed30c6ca137b
huanshenyi <txy1226052@gmail.com>
2022-05-03 14:54:05 +0900

    login状態の永久化

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import rootStore from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 import "antd/dist/antd.css";
+import { Spin } from "antd";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
+  <Provider store={rootStore.store}>
+    <PersistGate loading={<Spin />} persistor={rootStore.persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -9,6 +9,14 @@ import { myRecruitmentSlice } from "./myRecruitment/slice";
 import { alertControlSlice } from "./alertControl/slice";
 import { recruitmentItemSlice } from "./recruitmentItem/slice";
 import { userSlice } from "./user/slice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user"],
+};
 
 const rootReducer = combineReducers({
   myRecruitment: myRecruitmentSlice.reducer,
@@ -17,13 +25,18 @@ const rootReducer = combineReducers({
   user: userSlice.reducer,
 });
 
+// データ永久化処理
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 // const store = createStore(rootReducer);
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => [...getDefaultMiddleware()], //カスタムミドルウェアも追加可能
   devTools: true,
 });
 
+const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof store.getState>;
 
-export default store;
+export default { store, persistor };
